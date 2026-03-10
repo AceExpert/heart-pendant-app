19cb4c3a892242b56f1b13cfdbbaa919852926ae
sputh-the-pigeon <spuththepigeon@gmail.com>
2026-03-10 10:41:43 +0530

    ...

--- android/app/src/main/java/com/sayutel/heart/bluetooth/HeartService.java
+++ android/app/src/main/java/com/sayutel/heart/bluetooth/HeartService.java
@@ -10,6 +10,7 @@ import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.companion.AssociationInfo;
 import android.companion.AssociationRequest;
@@ -18,10 +19,12 @@ import android.companion.CompanionDeviceManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -49,6 +52,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Objects;
+import java.util.concurrent.Executor;
 import java.util.regex.Pattern;
 
 public class HeartService extends Service {
@@ -68,14 +72,21 @@ public class HeartService extends Service {
     NotificationManager notificationManager;
 
     public class MPendant {
-        public static final String device_secret = "";
+        public static final String device_secret = "5V2LVUexNoh4d2t";
+        public static final String device_id = "n7xqu7PT35";
+        public static final byte[] device_addr = new byte[]{(byte) 0x9c, 0x13, (byte) 0x9e, (byte) 0xab, (byte) 0xde, 0x12};
+        public static final String dev_addr = "9C:13:9E:AB:DE:12";
     }
 
     public class SPendant {
-        public static final String device_secret = "";
+        public static final String device_secret = "gHSvtEV8keKTwHK";
+        public static final String device_id = "EiRvXJHfKU";
+        public static final byte[] device_addr = new byte[]{(byte) 0x9c, 0x13, (byte) 0x9e, (byte) 0xab, (byte) 0xde, (byte) 0xee};
+        public static final String dev_addr = "9C:13:9E:AB:DE:EE";
     }
 
     public class Pendant {
+        public boolean shaleen_p = false;
         public String device_id = "";
         public String device_secret = "";
         public byte[] address = new byte[6];
@@ -109,6 +120,7 @@ public class HeartService extends Service {
         }
 
         public void startSyncingHeart() {
+            Log.i("sock sync", "sync heart");
             openUDPChannel();
         }
     }
@@ -129,9 +141,10 @@ public class HeartService extends Service {
         public void run() {
             try {
                 SocketChannel socketChannel = SocketChannel.open();
-                socketChannel.configureBlocking(false);
-                boolean connected = socketChannel.connect(new InetSocketAddress("0.0.0.0", 5660));
+                socketChannel.configureBlocking(true);
+                boolean connected = socketChannel.connect(new InetSocketAddress("209.74.79.245", 5660));
                 if(connected) {
+                    Log.i("socket", "conn");
                     int secret_len = pendantDevice.device_secret.length();
                     int fb_tok_len = pendantDevice.firebase_token.length();
                     ByteBuffer byteBuffer = ByteBuffer.allocate(1 + 2 + 6 + 2 + secret_len);
@@ -141,18 +154,22 @@ public class HeartService extends Service {
                     for(byte ch : pendantDevice.device_secret.getBytes()) {
                         byteBuffer.put(ch);
                     };
-                    socketChannel.write(byteBuffer);
+                    byteBuffer.rewind();
+                    int written = socketChannel.write(byteBuffer);
+                    Log.i("socket", String.valueOf(written));
                     byteBuffer = ByteBuffer.allocate(1 + 2 + 2 + fb_tok_len);
                     byteBuffer.put(new byte[]{1, 1, 0});
                     byteBuffer.put(new byte[]{(byte) (fb_tok_len & 0xff), (byte) ((fb_tok_len >> 8) & 0xff)});
                     for(byte ch : pendantDevice.firebase_token.getBytes()) {
                         byteBuffer.put(ch);
                     };
-                    socketChannel.write(byteBuffer);
+                    byteBuffer.rewind();
+                    written = socketChannel.write(byteBuffer);
+                    Log.i("socket", String.valueOf(written));
                     socketChannel.close();
                 }
             } catch (IOException e) {
-
+                Log.i("socket err", e.getLocalizedMessage());
             }
         }
     }
@@ -161,6 +178,8 @@ public class HeartService extends Service {
 
         public void process_cmd(String cmd) {
             if(!cmd.equals("ack")) {
+                Log.i("sock cmd", cmd);
+                Log.i("sock cmd len", String.valueOf(cmd.length()));
                 pendantDevice.udp.last_msg_time = LocalDateTime.now();
             }
 
@@ -191,14 +210,19 @@ public class HeartService extends Service {
                             break;
                         }
                     }
-                    sendUDPCmd("sync");
+                    if(pendantDevice.udp.last_ack_time == null || pendantDevice.udp.last_ack_time.until(LocalDateTime.now(), ChronoUnit.SECONDS) > 20) {
+                        sendUDPCmd("sync");
+                    }
                     if(pendantDevice.udp.selector.select(7000) > 0) {
+                        Log.i("sock", "selecting");
                         ByteBuffer buffer = ByteBuffer.allocate(512);
                         SocketAddress addr = pendantDevice.udp.channel.receive(buffer);
                         buffer.rewind();
                         String cmd = "";
                         while (buffer.hasRemaining()) {
-                            cmd += new String(new byte[]{buffer.get()}, StandardCharsets.UTF_8);
+                            byte chr = buffer.get();
+                            if(chr == 0) break;
+                            cmd += new String(new byte[]{chr}, StandardCharsets.UTF_8);
                         }
                         process_cmd(cmd);
                     }
@@ -221,6 +245,9 @@ public class HeartService extends Service {
                 sendUDPCmd("sync");
                 sendUDPCmd("sync");
                 (new UDPHeartClientTask()).start();
+            } else {
+                sendUDPCmd("sync");
+                sendUDPCmd("sync");
             }
         } catch (IOException e) {
 
@@ -244,7 +271,7 @@ public class HeartService extends Service {
     public void sendUDPCmd(String cmd) {
         try {
             pendantDevice.udp.last_sent_msg_time = LocalDateTime.now();
-            pendantDevice.udp.channel.send(getUDPCmd(cmd), new InetSocketAddress("0.0.0.0", 5600));
+            pendantDevice.udp.channel.send(getUDPCmd(cmd), new InetSocketAddress("209.74.79.245", 5600));
             pendantDevice.udp.last_write_time = LocalDateTime.now();
         } catch (IOException e) {
 
@@ -253,7 +280,7 @@ public class HeartService extends Service {
 
     public void sendUDPCmd(ByteBuffer cmd) {
         try {
-            pendantDevice.udp.channel.send(cmd, new InetSocketAddress("0.0.0.0", 5600));
+            pendantDevice.udp.channel.send(cmd, new InetSocketAddress("209.74.79.245", 5600));
             pendantDevice.udp.last_write_time = LocalDateTime.now();
         } catch (IOException e) {
 
@@ -278,6 +305,10 @@ public class HeartService extends Service {
         manager = getSystemService(BluetoothManager.class);
         adapter = manager.getAdapter();
         notificationManager = getSystemService(NotificationManager.class);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(broadcastReceiver, intentFilter, Context.RECEIVER_EXPORTED);
     }
 
     @Override
@@ -294,8 +325,8 @@ public class HeartService extends Service {
                 .setContentText("Making sure your hearts are always connected")
                 .build();
         startForeground(6, serviceNotif, ServiceInfo.FOREGROUND_SERVICE_TYPE_CONNECTED_DEVICE);
-        //if(adapter.isEnabled()) initBleClient();
-        showFCMToken();
+        if(adapter.isEnabled()) initBleClient();
+        //showFCMToken();
         return START_STICKY;
     }
 
@@ -321,6 +352,10 @@ public class HeartService extends Service {
         CompanionDeviceManager companionDeviceManager = (CompanionDeviceManager) getSystemService(Context.COMPANION_DEVICE_SERVICE);
         for(AssociationInfo info : companionDeviceManager.getMyAssociations()) {
             pendantAddress = info.getDeviceMacAddress().toString().toUpperCase();
+            int i = 0;
+            for(byte b : info.getDeviceMacAddress().toByteArray()) {
+                pendantDevice.address[i++] = b;
+            }
             break;
         }
 
@@ -330,22 +365,39 @@ public class HeartService extends Service {
             AssociationRequest request = new AssociationRequest.Builder()
                     .setDeviceProfile(AssociationRequest.DEVICE_PROFILE_WATCH)
                     .addDeviceFilter(new BluetoothLeDeviceFilter.Builder()
-                            .setNamePattern(Pattern.compile("'s heart$", Pattern.CASE_INSENSITIVE))
+                            .setNamePattern(Pattern.compile("^.+'s Heart$", Pattern.CASE_INSENSITIVE))
                             .build()
                     ).build();
 
-            companionDeviceManager.associate(request, getMainExecutor(), new CompanionDeviceManager.Callback() {
+            Executor executor = new Executor() {
+                @Override
+                public void execute(Runnable command) {
+                    command.run();
+                }
+            };
+
+            companionDeviceManager.associate(request, executor, new CompanionDeviceManager.Callback() {
 
                 @Override
                 public void onAssociationPending(@NonNull IntentSender intentSender) {
                     super.onAssociationPending(intentSender);
+                    try {
+                        intentSender.sendIntent(HeartService.this, 1, null, new IntentSender.OnFinished() {
+                            @Override
+                            public void onSendFinished(IntentSender IntentSender, Intent intent, int resultCode, String resultData, Bundle resultExtras) {
+
+                            }
+                        }, null);
+                    } catch (IntentSender.SendIntentException e) {
+                        Log.e("CDM error", e.toString());
+                    }
                 }
 
                 @Override
                 public void onAssociationCreated(@NonNull AssociationInfo associationInfo) {
                     int i = 0;
                     for(byte b : associationInfo.getDeviceMacAddress().toByteArray()) {
-                        pendantDevice.address[i] = b;
+                        pendantDevice.address[i++] = b;
                     }
                     pendantAddress = associationInfo.getDeviceMacAddress().toString().toUpperCase();
                     pendantConnect();
@@ -353,7 +405,7 @@ public class HeartService extends Service {
 
                 @Override
                 public void onFailure(@Nullable CharSequence charSequence) {
-
+                    Log.i("assoc", "fail " + charSequence.toString());
                 }
             });
         }
@@ -372,6 +424,16 @@ public class HeartService extends Service {
 
     public void sendPendantCmd(String cmd) {
         if(pendantDevice.connected) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             int resp = gattClient.writeCharacteristic(getMainCharac(), cmd.getBytes(), BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
             if(resp != 0) {
 
@@ -380,6 +442,17 @@ public class HeartService extends Service {
     }
 
     public void pendantConnect() {
+        if(Objects.equals(pendantAddress, MPendant.dev_addr)) {
+            pendantDevice.device_id = MPendant.device_id;
+            pendantDevice.device_secret = MPendant.device_secret;
+            pendantDevice.shaleen_p = false;
+        } else if (Objects.equals(pendantAddress, SPendant.dev_addr)) {
+            pendantDevice.device_id = SPendant.device_id;
+            pendantDevice.device_secret = SPendant.device_secret;
+            pendantDevice.shaleen_p = true;
+        }
+
+        showFCMToken();
         BluetoothDevice device = adapter.getRemoteDevice(pendantAddress);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
@@ -402,7 +475,7 @@ public class HeartService extends Service {
                     gatt.requestMtu(200);
                 } else {
                     pendantDevice.connected = false;
-                    closeUDPChannel();
+                    //closeUDPChannel();
                     if(adapter.isEnabled()) {
                         gatt.connect();
                     } else {
@@ -420,6 +493,17 @@ public class HeartService extends Service {
             @Override
             public void onServicesDiscovered(BluetoothGatt gatt, int status) {
                 super.onServicesDiscovered(gatt, status);
+                /*for(BluetoothGattService service : gatt.getServices()) {
+                    Log.i("ble service", service.getUuid().toString());
+                    for(BluetoothGattCharacteristic characteristic : service.getCharacteristics()) {
+                        Log.i("ble charac", characteristic.getUuid().toString());
+                        Log.i("ble charac perm", String.valueOf(characteristic.getPermissions()));
+                        for(BluetoothGattDescriptor descriptor : characteristic.getDescriptors()) {
+                            Log.i("ble descript", descriptor.getUuid().toString());
+                            Log.i("ble descript perm", String.valueOf(descriptor.getPermissions()));
+                        }
+                    }
+                }*/
                 gatt.setCharacteristicNotification(getMainCharac(), true);
                 gatt.writeDescriptor(getMainDescr(), BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
             }
